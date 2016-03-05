55c5514661604371bdbc67ca18dd7c80e555e14c
aryalg1 <aryalg1@nku.edu>
2016-03-05 14:32:27 -0500

    Update Node.java

--- src/edu/csc660/prog1/client/Node.java
+++ src/edu/csc660/prog1/client/Node.java
@@ -7,21 +7,22 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Node implements Runnable {
-	private String hostname = "localhost";
-	private int portNumber = 4321;
+	private static final String HOST_NAME = "localhost";
+	private static final int PORT_NUMBER = 4321;
 
 	@Override
 	public void run() {
 		Socket nodeSocket;
 		PrintWriter out;
+		
 		try {
-			nodeSocket = new Socket(hostname, 4321);
+			nodeSocket = new Socket(HOST_NAME, PORT_NUMBER);
 			System.out.println("Node connected");
 			out = new PrintWriter(nodeSocket.getOutputStream(), true);
 			BufferedReader in = new BufferedReader(new InputStreamReader(nodeSocket.getInputStream()));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		}
+		catch (IOException e) {
+			System.out.println("IOException was caused in the run() method of the Node class.");
 		}
 	}
 }
