cd9897641b5f07e88fb6fdb53d13bc0600e1fe7c
aryalg1 <aryalg1@nku.edu>
2016-03-05 14:33:26 -0500

    Update Client.java

--- src/edu/csc660/prog1/client/Client.java
+++ src/edu/csc660/prog1/client/Client.java
@@ -4,13 +4,13 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Client {
+	
+	public static final int NUMBER_OF_NODES = 10;
 
 	public static void main(String[] args) {
-		ExecutorService pool = Executors.newFixedThreadPool(10);
-		for(int i = 0; i < 10; i++){
+		ExecutorService pool = Executors.newFixedThreadPool(NUMBER_OF_NODES);
+		for(int i = 0; i < NUMBER_OF_NODES; i++){
 			pool.execute(new Node());
 		}
-
 	}
-
 }
